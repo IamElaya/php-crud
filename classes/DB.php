ce69fe86d6460aed2a9ba73a54e709710d56c4ac
Elaya Sison <asison13@gmail.com>
2016-11-08 14:36:47 -0800

    Sanitize/Config/Init

---
+++ classes/DB.php
@@ -0,0 +1,12 @@
+<?php
+class DB {
+    private static $instance = null;
+    private $_pdo, 
+            $_query, 
+            $_error = false, 
+            $_results, 
+            $_count = 0;
+
+
+
+}
