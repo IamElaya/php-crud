e5b724aa31558798952a9e91c24e83686ea84c2e
Elaya Sison <asison13@gmail.com>
2016-11-08 17:11:29 -0800

    First and Results functions

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
 require_once 'core/init.php';
 
-var_dump(Config::get('mysql/host')); // '127.0.0.1'
+// var_dump(Config::get('mysql/host')); // '127.0.0.1'
+
+// $users = DB::getInstance()->get('users', array('username', '=', 'alex'));
+// if($users->count()) {
+//     foreach($users as $user) {
+//         echo $user->username;
+//     }
+// }
+
+$user = DB::getInstance()->query("SELECT * FROM users");
+
+if($user->nocount()) {
+    echo 'No user';
+} else {
+    echo $user->first()->username;
+}
+// $db = new DB();
